26bc4b1608b7136ff510f05856becfd18c31de8f
fbleite <feliperbleite@gmail.com>
2019-04-10 23:16:22 -0400

    Updated frontend (Yeah I know, this is not using the fancy angular, but gets the job done too) with ranked player tables and unranked player table.
    Adds new players and stores them in the backend as well

---
+++ backend-sports-ladder/src/main/resources/static/script.js
@@ -0,0 +1,82 @@
+function init() {
+    getPlayers();
+};
+
+
+
+window.onload = init;
+
+function getPlayers() {
+    fetch('/player/')
+        .then(response => response.json())
+        .then(players => {
+            addPlayersToTables(players);
+        });
+
+}
+
+
+function addPlayersToTables(players) {
+    for (player in players) {
+        if (isPlayerRanked(players)) {
+            addToRankedTable(players[player].name, players[player].rank);
+        }
+        else {
+            addToUnrankedTable(players[player].name);
+        }
+    }
+
+    function isPlayerRanked(players) {
+        return players[player].rank != null;
+    }
+}
+
+function addToRankedTable(name, rank) {
+    var tableSelectorId = "rank";
+    var table = document.getElementById(tableSelectorId);
+    var row = table.insertRow(table.rows.length);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = rank;
+    cell2.innerHTML = name;
+}
+
+function addToUnrankedTable(name) {
+    var tableSelectorId = "unrank";
+    var table = document.getElementById(tableSelectorId);
+    var row = table.insertRow(table.rows.length);
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = name;
+}
+
+function addPlayerTextBox() {
+    toggleVisibilityPlayerAdding();
+}
+
+
+function toggleVisibilityPlayerAdding() {
+    for (var i = 0; i < document.getElementsByClassName("showToSavePlayer").length; i++) {
+        toggleIndividual(document.getElementsByClassName("showToSavePlayer")[i]);
+    }
+    toggleIndividual(document.getElementsByClassName("hideToSavePlayer")[0]);
+}
+
+
+function toggleIndividual(element) {
+    if (element.style.display === "none") {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+}
+
+function saveNewPlayer() {
+    var newPlayerName = document.getElementById("newPlayerName").value;
+    if (newPlayerName != "") {
+    addToUnrankedTable(newPlayerName);
+    toggleVisibilityPlayerAdding();
+    fetch ('/player/add/'+ newPlayerName+'/')
+        .catch(error => console.log(error));
+    document.getElementById("newPlayerName").value = "";
+    }
+}
